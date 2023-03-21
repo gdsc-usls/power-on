8c3bbbe4090713ed5aa5f0ded12b68ff28ae4899
Josh Daniel <joshxfi.dev@gmail.com>
2023-03-21 09:53:52 +0800

    feat: display 3d certificate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import Tilt from "react-parallax-tilt";
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="bg-slate-900 min-h-screen flex flex-col items-center justify-center">
+      <Tilt>
+        <div className="w-[800px] reltive rounded-lg grid place-items-center overflow-hidden">
+          <p className='absolute text-5xl text-gray-600 font-bold mt-16'>JOSH DANIEL BAÑARES</p>
+          <img
+            className="w-full h-full object-cover"
+            src="/assets/poweron_cert.png"
+            alt="PowerOn Certificate"
+          />
+        </div>
+      </Tilt>
     </div>
   );
 }
