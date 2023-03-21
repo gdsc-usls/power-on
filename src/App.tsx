58a7c104a56d4a36f7fbb1f4c4704ef71238d3ea
Josh Daniel <joshxfi.dev@gmail.com>
2023-03-21 10:02:51 +0800

    chore: add google font

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="bg-slate-900 min-h-screen flex flex-col items-center justify-center">
       <Tilt>
-        <div className="w-[800px] reltive rounded-lg grid place-items-center overflow-hidden">
-          <p className='absolute text-5xl text-gray-600 font-bold mt-16'>JOSH DANIEL BAÑARES</p>
+        <div className="w-[750px] reltive rounded-lg grid place-items-center overflow-hidden">
+          <p className='absolute font-google text-[40px] text-[#7f858c] font-bold mt-20'>JOSH DANIEL BAÑARES</p>
           <img
             className="w-full h-full object-cover"
             src="/assets/poweron_cert.png"
