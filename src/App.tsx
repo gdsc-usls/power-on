c220cabe3bdeeed24950b7569037f88523d2ac3a
Josh Daniel <joshxfi.dev@gmail.com>
2023-03-21 11:17:15 +0800

    chore: add gradient in bg

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,23 @@ import Tilt from "react-parallax-tilt";
 
 function App() {
   return (
-    <div className="bg-slate-900 min-h-screen flex flex-col items-center justify-center">
-      <Tilt>
-        <div className="w-[750px] reltive rounded-lg grid place-items-center overflow-hidden">
+    <div className="bg-slate-900 min-h-screen flex flex-col relative items-center justify-center">
+      <Tilt className="z-10">
+        <div className="w-[750px] relative rounded-lg grid place-items-center overflow-hidden">
           <p className='absolute font-google text-[40px] text-[#7f858c] font-bold mt-20'>JOSH DANIEL BAÑARES</p>
           <img
-            className="w-full h-full object-cover"
+            className="w-full pointer-events-none h-full object-cover"
             src="/assets/poweron_cert.png"
             alt="PowerOn Certificate"
           />
         </div>
       </Tilt>
+
+          <img
+            className="w-full h-full object-cover absolute bottom-0"
+            src="/assets/gradient.png"
+            alt="PowerOn Certificate"
+          />
     </div>
   );
 }
