0d7ecf200b317e9b79c79309f49207194cceddbd
Josh Daniel <joshxfi.dev@gmail.com>
2023-03-21 11:55:59 +0800

    feat: add responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="bg-slate-900 min-h-screen flex flex-col relative items-center justify-center">
       <Tilt className="z-10">
-        <div className="w-[750px] relative rounded-lg grid place-items-center overflow-hidden">
-          <p className='absolute font-google text-[40px] text-[#7f858c] font-bold mt-20'>JOSH DANIEL BAÑARES</p>
+        <div className="md:w-[750px] sm:w-[600px] w-[420px] relative rounded-lg grid place-items-center overflow-hidden">
+          <p className='absolute font-google md:text-[40px] text-lg sm:text-[30px] text-[#7f858c] font-bold md:mt-20 sm:mt-16 mt-12'>JOSH DANIEL BAÑARES</p>
           <img
             className="w-full pointer-events-none h-full object-cover"
             src="/assets/poweron_cert.png"
