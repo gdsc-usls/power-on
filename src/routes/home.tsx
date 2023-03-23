7b8a84118c461ffab7b8996acb29f3e6e4e648b3
Josh Daniel <joshxfi.dev@gmail.com>
2023-03-23 09:57:56 +0800

    chore: update input ui

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -5,15 +5,20 @@ export const Home = () => {
   const [studentId, setStudentId] = useState("");
 
   return (
-    <div className="flex space-x-4 border rounded z-10 items-center text-white px-4">
+    <div className="bg-white z-10 flex space-x-4 border border-black rounded-lg items-center px-4 bg-opacity-80">
       <input
+        required
         type="text"
-        placeholder="Enter Student ID:"
+        maxLength={7}
+        placeholder="Enter Student ID"
         value={studentId}
         onChange={(e) => setStudentId(e.target.value)}
-        className="bg-none outline-none bg-transparent text-xl"
+        className="bg-none font-google outline-none bg-transparent text-xl"
       />
-      <Link to={`/certificate/${studentId}`} className="text-4xl mb-1">
+      <Link
+        to={studentId ? `/certificate/${studentId}` : "/"}
+        className="border-l border-black hover:text-blue-600 transition-colors text-4xl pl-4 pt-2 pb-3"
+      >
         &rarr;
       </Link>
     </div>
