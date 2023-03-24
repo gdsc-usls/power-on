82aaa207d443d806e89eacadd89fda88808ed431
Josh Daniel <joshxfi.dev@gmail.com>
2023-03-24 12:29:07 +0800

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className='bg-black py-4 px-6 rounded-full text-white font-google text-center bottom-5 absolute z-10'>
+      <p>Google Developer Student Clubs - USLS © 2023</p>
+    </footer>
+  );
+};
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import { Footer } from "../components";
 
 export const Root = () => {
   return (
     <div className="bg-black min-h-screen flex flex-col items-center justify-center">
       <Outlet />
 
+      <Footer />
       <img
         className="w-full object-cover h-full xl:h-auto absolute bottom-0 pointer-events-none"
         src="/assets/gradient.png"
